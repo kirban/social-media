1111e886b310c070bf154aaf1469141da1e2f0c7
Kirill Romanov <kirban95@gmail.com>
2026-07-07 11:09:04 +0300

    implemented createdialog & getdialogid & createmessage methods in repo

--- internal/model/dialog.go
+++ internal/model/dialog.go
@@ -27,3 +27,7 @@ type Dialog struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+type CreateDialogDTO struct {
+	Users [2]UserID
+}
--- internal/repository/dialog.go
+++ internal/repository/dialog.go
@@ -2,6 +2,9 @@ package repository
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"fmt"
 
 	"github.com/kirban/social-media/internal/db"
 	"github.com/kirban/social-media/internal/logger"
@@ -9,12 +12,12 @@ import (
 )
 
 type DialogRepositoryInterface interface {
-	CreateMessage(ctx context.Context, dialogID model.DialogID, from, to model.UserID, text string) (*model.DialogMessageID, error)
+	CreateMessage(ctx context.Context, dialogID *model.DialogID, from, to model.UserID, text string) (*model.DialogMessageID, error)
 	GetDialogID(ctx context.Context, srcUser, dstUser model.UserID) (*model.DialogID, error)
 	ListMessages(ctx context.Context, dialogID *model.DialogID) ([]model.DialogMessage, error)
+	CreateDialog(ctx context.Context, dto *model.CreateDialogDTO) (*model.DialogID, error)
 
 	GetByID(ctx context.Context, id model.DialogID) (*model.Dialog, error)
-	Create(ctx context.Context, dto any) (*model.DialogID, error)
 	Update(ctx context.Context) error
 	Delete(ctx context.Context) error
 	ListDialogs(ctx context.Context, userID model.UserID) ([]model.Dialog, error)
@@ -32,14 +35,67 @@ func NewDialogRepository(c *db.Cluster, l *logger.AppLogger) *DialogRepository {
 	}
 }
 
-func (r *DialogRepository) CreateMessage(ctx context.Context, dialogID model.DialogID, from, to model.UserID, text string) (*model.DialogMessageID, error) {
-	return nil, nil
+func (r *DialogRepository) CreateMessage(ctx context.Context, dialogID *model.DialogID, from, to model.UserID, text string) (*model.DialogMessageID, error) {
+	var messID model.DialogMessageID
+	err := r.cluster.Master().QueryRowContext(ctx, `INSERT INTO "dialog_message" ("dialog_id", "from", "to", "text")
+		VALUES ($1, $2, $3, $4)
+		RETURNING id;
+	`, dialogID, from, to, text).Scan(&messID)
+	if err != nil {
+		return nil, fmt.Errorf("failed create message: %w", err)
+	}
+	return &messID, nil
 }
 
 func (r *DialogRepository) GetDialogID(ctx context.Context, srcUser, dstUser model.UserID) (*model.DialogID, error) {
-	return nil, nil
+	var dialogID model.DialogID
+	err := r.cluster.Replica().QueryRowContext(
+		ctx,
+		`SELECT dialog_id
+		FROM "dialog_user"
+		WHERE user_id IN ($1::UUID, $2::UUID)
+		GROUP BY dialog_id
+		HAVING COUNT(DISTINCT user_id) = 2;`, srcUser, dstUser).Scan(&dialogID)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+		return nil, err
+	}
+
+	return &dialogID, nil
 }
 
 func (r *DialogRepository) ListMessages(ctx context.Context, dialogID *model.DialogID) ([]model.DialogMessage, error) {
 	return nil, nil
+}
+
+func (r *DialogRepository) CreateDialog(ctx context.Context, dto *model.CreateDialogDTO) (*model.DialogID, error) {
+	txOpts := &sql.TxOptions{
+		Isolation: sql.LevelDefault,
+		ReadOnly:  false,
+	}
+	tx, err := r.cluster.Master().BeginTx(ctx, txOpts)
+	if err != nil {
+		return nil, fmt.Errorf("failed to begin tx: %w", err)
+	}
+	defer tx.Rollback()
+
+	var dialogID model.DialogID
+	err = tx.QueryRowContext(ctx, `INSERT INTO "dialog" DEFAULT VALUES RETURNING id;`).Scan(&dialogID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert dialog: %w", err)
+	}
+
+	bulkInsertQ := `INSERT INTO "dialog_user" (dialog_id, user_id) VALUES ($1, $2), ($1, $3);`
+	_, err = tx.ExecContext(ctx, bulkInsertQ, dialogID, dto.Users[0], dto.Users[1])
+	if err != nil {
+		return nil, fmt.Errorf("failed to insert dialog users: %w", err)
+	}
+	if err := tx.Commit(); err != nil {
+		return nil, fmt.Errorf("failed to commit tx: %w", err)
+	}
+
+	return &dialogID, nil
 }
